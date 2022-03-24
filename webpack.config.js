fe2cb284d4745917d9fe04dd17bda4d165c3f79c
Alexander Shchukin <1340068@gmail.com>
2022-03-24 13:32:05 +0300

    add posthtml with templates

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,36 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const glob = require('glob')
+const globParent = require('glob-parent')
 
 const devMode = process.env.NODE_ENV !== 'production'
 const mode = devMode ? 'development' : 'production'
+
+
+
+
+// ================== Begin setup posthtml ========================= //
+const htmlRoot = './src/*.html'
+const posthtmlInclude = require('posthtml-include')({ root: './src/partials' })
+
+const files = glob.sync(htmlRoot)
+const htmlRootDir = globParent(htmlRoot)
+
+const htmlWebpackPlugins = files.map(file => {
+  const hwpConfig = {
+    filename: path.relative(htmlRootDir, file),
+    template: file,
+    inject: true,
+    minify: false,
+    hash: true,
+  }
+  return new HtmlWebpackPlugin(hwpConfig)
+})
+// ================== End setup posthtml ========================= //
+
+
+
 
 module.exports = {
   entry: './src/index.js',
@@ -12,12 +39,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
-    // assetModuleFilename: 'images/[name][ext][query]',
   },
 
   mode: mode,
 
   devtool: devMode ? 'source-map' : false,
+
+  // devServer: {
+  //   open: true,
+  //   contentBase: [path.resolve(__dirname, 'dist')]
+  // },
+  // devServer: {
+  //   hot: true,
+  // },
 
   module: {
     rules: [
@@ -40,12 +74,31 @@ module.exports = {
         test: /\.svg/,
         type: 'asset/inline',
       },
+      {
+        test: /\.html$/,
+        use: [
+          'html-loader',
+          {
+            loader: 'posthtml-loader',
+            options: {
+              plugins: [
+                posthtmlInclude,
+              ],
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.js?$/,
+      //   loader: 'babel-loader',
+      // },
     ],
   },
 
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    // new HtmlWebpackPlugin({ template: './src/index.html' }),
+    ...htmlWebpackPlugins,
     new MiniCssExtractPlugin(),
   ],
 
